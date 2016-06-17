4610b99a94efb511de45c16a08a0c4871d105a4a
Alan Gutierrez <alan@prettyrobots.com>
2016-06-17 07:44:34 -0500

    Thoughts on streaming.

---
+++ notes.js
@@ -0,0 +1,16 @@
+cadence(function () {
+    ua.fetch({
+        url: 'http://127.0.0.1:8080',
+        post: input = new stream.PassThrough
+        response: 'application/jsonstream'
+    }, async())
+}, function (response) {
+    var staccato = new Staccato(response)
+    var loop = async(function () {
+        staccato.read(async())
+    }, function (json) {
+        if (json == null) {
+            return [ loop.break ]
+        }
+    })()
+})
