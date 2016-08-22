8c56d987d80ef4bd9da1e109ffe747e6fe0ea42c
Alan Gutierrez <alan@prettyrobots.com>
2016-08-22 07:19:41 -0500

    Debugging socket hang up.

--- http.js
+++ http.js
@@ -12,16 +12,28 @@ Transport.prototype.send = cadence(function (async, request) {
         http = require('http')
     }
     var client = http.request(request.options)
-    delta(async()).ee(client).on('response')
-    if (request.payload) {
-        client.write(request.payload)
-    }
-    if (request.timeout) {
-        client.setTimeout(request.timeout, function () {
-            client.abort()
+    async(function () {
+        delta(async()).ee(client).on('response')
+        if (request.payload) {
+            client.write(request.payload)
+        }
+        if (request.timeout) {
+            client.setTimeout(request.timeout, function () {
+                client.abort()
+            })
+        }
+        client.end()
+    }, function (response) {
+        client.once('error', function (stack) {
+            // What do to about? http://stackoverflow.com/a/16995223/90123
+            console.error('request swallowed error', {
+                url: request.url,
+                headers: request.headers,
+                stack: error.stack
+            })
         })
-    }
-    client.end()
+        return [ response ]
+    })
 })
 
 module.exports = Transport
