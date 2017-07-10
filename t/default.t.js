1f02e89d702a69d3c5fc5d92ae68c8d8b5b5e2d1
Alan Gutierrez <alan@prettyrobots.com>
2017-07-09 21:54:11 -0500

    100% test coverage of default.

--- t/default.t.js
+++ t/default.t.js
@@ -1,8 +1,34 @@
-require('proof')(1, prove)
+require('proof')(8, prove)
 
 function prove (okay) {
     var expand = require('../default')
     okay(expand({
         url: 'http://127.0.0.1:8888/path'
     }).path, '/path', 'path')
+    okay(expand({
+        url: 'http://127.0.0.1:8888/path'
+    }).identifier, '127.0.0.1:8888', 'ip identifier')
+    okay(expand({
+        socketPath: '/var/sock'
+    }).identifier, '/var/sock', 'domain socket identifier')
+    okay(expand({
+        url: 'http://127.0.0.1:8888',
+        token: 'x'
+    }).headers.authorization, 'Bearer x', 'token')
+    okay(expand({
+        url: 'http://127.0.0.1:8888',
+        response: 'stream'
+    }).response, 'stream', 'response')
+    okay(expand({
+        url: 'http://127.0.0.1:8888',
+        payload: {}
+    }).payload.toString(), '{}', 'json')
+    okay(expand({
+        url: 'http://127.0.0.1:8888',
+        plugins: [ {} ]
+    }).plugins, [ {} ], 'plugins')
+    okay(expand({
+        url: 'http://127.0.0.1:8888',
+        method: 'DELETE'
+    }).method, 'DELETE', 'method')
 }
