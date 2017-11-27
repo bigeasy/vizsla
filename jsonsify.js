311bd63bb9818385a0961b4013ff9497f9a349bd
Alan Gutierrez <alan@prettyrobots.com>
2017-11-27 02:18:17 -0600

    Create JSON-stream gateway.

---
+++ jsonsify.js
@@ -0,0 +1,26 @@
+var cadence = require('cadence')
+
+var byline = require('byline')
+var errorify = require('./errorify')
+
+var coalesce = require('extant')
+var createSelector = require('./select')
+var util = require('util')
+
+var JsonStream = require('./jsons')
+
+var Parser = require('./parser')
+
+function Jsonpify (options) {
+    options.when = [ 'content-type: application/json' ].concat(coalesce(options.when, []))
+    Parser.call(this, options, [])
+}
+util.inherits(Jsonpify, Parser)
+
+Jsonpify.prototype._parse = cadence(function (async, body, response) {
+    return [ body.pipe(byline()).pipe(new JsonStream()), response ]
+})
+
+module.exports = function (options) {
+    return new Jsonpify(options)
+}
---
+++ t/jsonsify.t.js
@@ -0,0 +1,31 @@
+require('proof')(1, require('cadence')(prove))
+
+function prove (async, okay) {
+    var cadence = require('cadence')
+
+    var jsonsify = require('../jsonsify')
+    var requestify = require('./requestify')
+
+    var delta = require('delta')
+    var gateway = jsonsify({ when: [ 200 ] })
+
+    var Descent = require('../descent')
+    var descent
+
+    async(function () {
+        descent = new Descent([{
+            gateways: [ gateway, requestify('{}\n', {
+                statusCode: 200,
+                type: {
+                    type: 'application',
+                    subtype: 'json-stream'
+                }
+            }).gateways.shift() ]
+        }])
+        descent.descend(async())
+    }, function (body, response) {
+        delta(async()).ee(body).on('data', []).on('end')
+    }, function (data) {
+        okay(data, [{}], 'json')
+    })
+}
