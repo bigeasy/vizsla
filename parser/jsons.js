61f3b0f08d40843ce14c1df052d20998eff2ca67
Alan Gutierrez <alan@prettyrobots.com>
2018-05-16 23:58:42 -0500

    Combine JSON streaming into one module.

--- parser/jsons.js
+++ parser/jsons.js
@@ -1,6 +1,20 @@
 var cadence = require('cadence')
 var byline = require('byline')
-var JsonStream = require('../jsons')
+
+var util = require('util')
+var stream = require('stream')
+
+function JsonStream () {
+    stream.Transform.call(this, { objectMode: true })
+}
+util.inherits(JsonStream, stream.Transform)
+
+JsonStream.prototype._transform = cadence(function (async, chunk, encoding) {
+    this.push(JSON.parse(chunk.toString()))
+    return []
+})
+
+JsonStream.prototype._flush = cadence(function () { return [] })
 
 function StreamingJsonParser (options) {
     this.options = options
